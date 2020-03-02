ac80b0aeba155ea2706df1153281299541827bcb
Brandon Weaver <a7xncob@gmail.com>
2020-03-02 02:15:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-class Dog {
-  constructor(name, breed) {
-    this.name = name;
-    this.breed = breed;
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
   }
 }
